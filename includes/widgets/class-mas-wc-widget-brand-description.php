4b8780f965ba7e1389c914378ab87e3979b0c48a
Ibrahim Ibn Dawood <ibrahim@transvelo.com>
2022-02-14 14:04:50 +0530

    WP Standards

--- includes/widgets/class-mas-wc-widget-brand-description.php
+++ includes/widgets/class-mas-wc-widget-brand-description.php
@@ -4,21 +4,47 @@
  *
  * When viewing a brand archive, show the current brands description + image
  *
- * @category	Widgets
- * @version		1.0.0
- * @author      Madras Themes
+ * @version  1.0.5
+ * @package  Mas_WC_Brands/Widgets
  */
 
 if ( ! class_exists( 'Mas_WC_Widget_Brand_Description' ) ) {
+	/**
+	 * Brand Description widget.
+	 */
 	class Mas_WC_Widget_Brand_Description extends WP_Widget {
 
-		/** Variables to setup the widget. */
-		var $woo_widget_cssclass;
-		var $woo_widget_description;
-		var $woo_widget_idbase;
-		var $woo_widget_name;
+		/**
+		 * CSS class.
+		 *
+		 * @var string
+		 */
+		public $woo_widget_cssclass;
 
-		/** constructor */
+		/**
+		 * Widget description.
+		 *
+		 * @var string
+		 */
+		public $woo_widget_description;
+
+		/**
+		 * Widget ID.
+		 *
+		 * @var string
+		 */
+		public $woo_widget_idbase;
+
+		/**
+		 * Widget name.
+		 *
+		 * @var string
+		 */
+		public $woo_widget_name;
+
+		/**
+		 * Constructor.
+		 */
 		public function __construct() {
 
 			/* Widget variable settings. */
@@ -28,27 +54,39 @@ if ( ! class_exists( 'Mas_WC_Widget_Brand_Description' ) ) {
 			$this->woo_widget_cssclass    = 'widget_brand_description';
 
 			/* Widget settings. */
-			$widget_ops = array( 'classname' => $this->woo_widget_cssclass, 'description' => $this->woo_widget_description );
+			$widget_ops = array(
+				'classname'   => $this->woo_widget_cssclass,
+				'description' => $this->woo_widget_description,
+			);
 
 			/* Create the widget. */
 			parent::__construct( $this->woo_widget_idbase, $this->woo_widget_name, $widget_ops );
 		}
 
-		/** @see WP_Widget */
+		/**
+		 * Output widget.
+		 *
+		 * @param array $args     Arguments.
+		 * @param array $instance Widget instance.
+		 *
+		 * @see WP_Widget
+		 */
 		public function widget( $args, $instance ) {
 			$brand_taxonomy = Mas_WC_Brands()->get_brand_taxonomy();
 
-			if( empty( $brand_taxonomy ) ) {
+			if ( empty( $brand_taxonomy ) ) {
 				return;
 			}
-			
+
 			extract( $args );
 
-			if ( ! is_tax( $brand_taxonomy ) )
+			if ( ! is_tax( $brand_taxonomy ) ) {
 				return;
+			}
 
-			if ( ! get_query_var( 'term' ) )
+			if ( ! get_query_var( 'term' ) ) {
 				return;
+			}
 
 			$thumbnail = '';
 			$term      = get_term_by( 'slug', get_query_var( 'term' ), $brand_taxonomy );
@@ -57,26 +95,49 @@ if ( ! class_exists( 'Mas_WC_Widget_Brand_Description' ) ) {
 
 			echo $before_widget . $before_title . $term->name . $after_title;
 
-			wc_get_template( 'widgets/brand-description.php', array(
-				'thumbnail' => $thumbnail,
-				'brand' => $term
-			), 'mas-woocommerce-brands', untrailingslashit( MAS_WCBR_ABSPATH ) . '/templates/' );
+			wc_get_template(
+				'widgets/brand-description.php',
+				array(
+					'thumbnail' => $thumbnail,
+					'brand'     => $term,
+				),
+				'mas-woocommerce-brands',
+				untrailingslashit( MAS_WCBR_ABSPATH ) . '/templates/'
+			);
 
 			echo $after_widget;
 		}
 
-		/** @see WP_Widget->update */
+		/**
+		 * Updates a particular instance of a widget.
+		 *
+		 * @see    WP_Widget->update
+		 * @param  array $new_instance New instance.
+		 * @param  array $old_instance Old instance.
+		 * @return array
+		 */
 		public function update( $new_instance, $old_instance ) {
-			$instance['title'] = strip_tags( stripslashes( $new_instance['title'] ) );
+			$instance['title'] = wp_strip_all_tags( stripslashes( $new_instance['title'] ) );
 			return $instance;
 		}
 
-		/** @see WP_Widget->form */
+		/**
+		 * Outputs the settings update form.
+		 *
+		 * @see   WP_Widget->form
+		 *
+		 * @param array $instance Instance.
+		 */
 		public function form( $instance ) {
 			?>
 				<p>
-					<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php esc_html_e('Title:', 'mas-wc-brands') ?></label>
-					<input type="text" class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" value="<?php if ( isset ( $instance['title'] ) ) echo esc_attr( $instance['title'] ); ?>" />
+					<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php esc_html_e( 'Title:', 'mas-wc-brands' ); ?></label>
+					<input type="text" class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" value="
+																	  <?php
+																		if ( isset( $instance['title'] ) ) {
+																			echo esc_attr( $instance['title'] );}
+																		?>
+					" />
 				</p>
 			<?php
 		}
--- includes/widgets/class-mas-wc-widget-brand-thumbnails.php
+++ includes/widgets/class-mas-wc-widget-brand-thumbnails.php
@@ -4,8 +4,8 @@
  *
  * Show brand images as thumbnails
  *
- * @category	Widgets
- * @version		1.0.0
+ * @category    Widgets
+ * @version     1.0.0
  * @author      Madras Themes
  */
 if ( ! class_exists( 'Mas_WC_Widget_Brand_Thumbnails' ) ) {
@@ -27,7 +27,10 @@ if ( ! class_exists( 'Mas_WC_Widget_Brand_Thumbnails' ) ) {
 			$this->woo_widget_cssclass    = 'widget_brand_thumbnails';
 
 			/* Widget settings. */
-			$widget_ops = array( 'classname' => $this->woo_widget_cssclass, 'description' => $this->woo_widget_description );
+			$widget_ops = array(
+				'classname'   => $this->woo_widget_cssclass,
+				'description' => $this->woo_widget_description,
+			);
 
 			/* Create the widget. */
 			parent::__construct( $this->woo_widget_idbase, $this->woo_widget_name, $widget_ops );
@@ -37,16 +40,25 @@ if ( ! class_exists( 'Mas_WC_Widget_Brand_Thumbnails' ) ) {
 		public function widget( $args, $instance ) {
 			$brand_taxonomy = Mas_WC_Brands()->get_brand_taxonomy();
 
-			if( empty( $brand_taxonomy ) ) {
+			if ( empty( $brand_taxonomy ) ) {
 				return;
 			}
-			
+
 			extract( $args );
 
 			$exclude = array_map( 'intval', explode( ',', $instance['exclude'] ) );
-			$order = $instance['orderby'] == 'name' ? 'asc' : 'desc';
+			$order   = $instance['orderby'] == 'name' ? 'asc' : 'desc';
 
-			$brands = get_terms( array( 'taxonomy' => $brand_taxonomy, 'hide_empty' => $instance['hide_empty'], 'orderby' => $instance['orderby'], 'exclude' => $exclude, 'number' => $instance['number'], 'order' => $order ) );
+			$brands = get_terms(
+				array(
+					'taxonomy'   => $brand_taxonomy,
+					'hide_empty' => $instance['hide_empty'],
+					'orderby'    => $instance['orderby'],
+					'exclude'    => $exclude,
+					'number'     => $instance['number'],
+					'order'      => $order,
+				)
+			);
 
 			if ( ! $brands || ! is_array( $brands ) ) {
 				return;
@@ -59,13 +71,18 @@ if ( ! class_exists( 'Mas_WC_Widget_Brand_Thumbnails' ) ) {
 				echo $args['before_title'] . $title . $args['after_title'];
 			}
 
-			wc_get_template( 'widgets/brand-thumbnails.php', array(
-				'taxonomy'      => $brand_taxonomy,
-				'brands'        => $brands,
-				'columns'       => $instance['columns'],
-				'image_size'	=> $instance['image_size'],
-				'fluid_columns' => ! empty( $instance['fluid_columns'] ) ? true : false,
-			), 'mas-woocommerce-brands', untrailingslashit( MAS_WCBR_ABSPATH ) . '/templates/' );
+			wc_get_template(
+				'widgets/brand-thumbnails.php',
+				array(
+					'taxonomy'      => $brand_taxonomy,
+					'brands'        => $brands,
+					'columns'       => $instance['columns'],
+					'image_size'    => $instance['image_size'],
+					'fluid_columns' => ! empty( $instance['fluid_columns'] ) ? true : false,
+				),
+				'mas-woocommerce-brands',
+				untrailingslashit( MAS_WCBR_ABSPATH ) . '/templates/'
+			);
 
 			echo $after_widget;
 		}
@@ -124,48 +141,79 @@ if ( ! class_exists( 'Mas_WC_Widget_Brand_Thumbnails' ) ) {
 
 			?>
 				<p>
-					<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php esc_html_e('Title:', 'mas-wc-brands') ?></label>
-					<input type="text" class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" value="<?php if ( isset ( $instance['title'] ) ) echo esc_attr( $instance['title'] ); ?>" />
+					<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php esc_html_e( 'Title:', 'mas-wc-brands' ); ?></label>
+					<input type="text" class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" value="
+																	  <?php
+																		if ( isset( $instance['title'] ) ) {
+																			echo esc_attr( $instance['title'] );}
+																		?>
+					" />
 				</p>
 
 				<p>
-					<label for="<?php echo $this->get_field_id( 'columns' ); ?>"><?php esc_html_e('Columns:', 'mas-wc-brands') ?></label>
-					<input type="text" class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'columns' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'columns' ) ); ?>" value="<?php if ( isset ( $instance['columns'] ) ) echo esc_attr( $instance['columns'] ); else echo '1'; ?>" />
+					<label for="<?php echo $this->get_field_id( 'columns' ); ?>"><?php esc_html_e( 'Columns:', 'mas-wc-brands' ); ?></label>
+					<input type="text" class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'columns' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'columns' ) ); ?>" value="
+																	  <?php
+																		if ( isset( $instance['columns'] ) ) {
+																			echo esc_attr( $instance['columns'] );
+																		} else {
+																			echo '1';
+																		}
+																		?>
+					" />
 				</p>
 
 				<p>
-					<label for="<?php echo $this->get_field_id( 'image_size' ); ?>"><?php esc_html_e('Image Size:', 'mas-wc-brands') ?></label>
-					<input type="text" class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'image_size' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'image_size' ) ); ?>" value="<?php if ( isset ( $instance['image_size'] ) ) echo esc_attr( $instance['image_size'] ); else echo ''; ?>" />
+					<label for="<?php echo $this->get_field_id( 'image_size' ); ?>"><?php esc_html_e( 'Image Size:', 'mas-wc-brands' ); ?></label>
+					<input type="text" class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'image_size' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'image_size' ) ); ?>" value="
+																	  <?php
+																		if ( isset( $instance['image_size'] ) ) {
+																			echo esc_attr( $instance['image_size'] );
+																		} else {
+																			echo '';
+																		}
+																		?>
+					" />
 				</p>
 
 				<p>
-					<label for="<?php echo $this->get_field_id( 'fluid_columns' ); ?>"><?php esc_html_e('Fluid columns:', 'mas-wc-brands') ?></label>
+					<label for="<?php echo $this->get_field_id( 'fluid_columns' ); ?>"><?php esc_html_e( 'Fluid columns:', 'mas-wc-brands' ); ?></label>
 					<input type="checkbox" <?php checked( $instance['fluid_columns'] ); ?> id="<?php echo esc_attr( $this->get_field_id( 'fluid_columns' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'fluid_columns' ) ); ?>" />
 				</p>
 
 				<p>
-					<label for="<?php echo $this->get_field_id( 'number' ); ?>"><?php esc_html_e('Number:', 'mas-wc-brands') ?></label>
-					<input type="text" class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'number' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'number' ) ); ?>" value="<?php if ( isset ( $instance['number'] ) ) echo esc_attr( $instance['number'] ); ?>" placeholder="<?php esc_html_e('All', 'mas-wc-brands'); ?>" />
+					<label for="<?php echo $this->get_field_id( 'number' ); ?>"><?php esc_html_e( 'Number:', 'mas-wc-brands' ); ?></label>
+					<input type="text" class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'number' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'number' ) ); ?>" value="
+																	  <?php
+																		if ( isset( $instance['number'] ) ) {
+																			echo esc_attr( $instance['number'] );}
+																		?>
+					" placeholder="<?php esc_html_e( 'All', 'mas-wc-brands' ); ?>" />
 				</p>
 
 				<p>
-					<label for="<?php echo $this->get_field_id( 'exclude' ); ?>"><?php esc_html_e('Exclude:', 'mas-wc-brands') ?></label>
-					<input type="text" class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'exclude' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'exclude' ) ); ?>" value="<?php if ( isset ( $instance['exclude'] ) ) echo esc_attr( $instance['exclude'] ); ?>" placeholder="<?php esc_html_e('None', 'mas-wc-brands'); ?>" />
+					<label for="<?php echo $this->get_field_id( 'exclude' ); ?>"><?php esc_html_e( 'Exclude:', 'mas-wc-brands' ); ?></label>
+					<input type="text" class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'exclude' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'exclude' ) ); ?>" value="
+																	  <?php
+																		if ( isset( $instance['exclude'] ) ) {
+																			echo esc_attr( $instance['exclude'] );}
+																		?>
+					" placeholder="<?php esc_html_e( 'None', 'mas-wc-brands' ); ?>" />
 				</p>
 
 				<p>
-					<label for="<?php echo $this->get_field_id( 'hide_empty' ); ?>"><?php esc_html_e('Hide empty brands:', 'mas-wc-brands') ?></label>
+					<label for="<?php echo $this->get_field_id( 'hide_empty' ); ?>"><?php esc_html_e( 'Hide empty brands:', 'mas-wc-brands' ); ?></label>
 					<select id="<?php echo esc_attr( $this->get_field_id( 'hide_empty' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'hide_empty' ) ); ?>">
-						<option value="1" <?php selected( $instance['hide_empty'], 1 ) ?>><?php esc_html_e('Yes', 'mas-wc-brands') ?></option>
-						<option value="0" <?php selected( $instance['hide_empty'], 0 ) ?>><?php esc_html_e('No', 'mas-wc-brands') ?></option>
+						<option value="1" <?php selected( $instance['hide_empty'], 1 ); ?>><?php esc_html_e( 'Yes', 'mas-wc-brands' ); ?></option>
+						<option value="0" <?php selected( $instance['hide_empty'], 0 ); ?>><?php esc_html_e( 'No', 'mas-wc-brands' ); ?></option>
 					</select>
 				</p>
 
 				<p>
-					<label for="<?php echo $this->get_field_id( 'orderby' ); ?>"><?php esc_html_e('Order by:', 'mas-wc-brands') ?></label>
+					<label for="<?php echo $this->get_field_id( 'orderby' ); ?>"><?php esc_html_e( 'Order by:', 'mas-wc-brands' ); ?></label>
 					<select id="<?php echo esc_attr( $this->get_field_id( 'orderby' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'orderby' ) ); ?>">
-						<option value="name" <?php selected( $instance['orderby'], 'name' ) ?>><?php esc_html_e('Name', 'mas-wc-brands') ?></option>
-						<option value="count" <?php selected( $instance['orderby'], 'count' ) ?>><?php esc_html_e('Count', 'mas-wc-brands') ?></option>
+						<option value="name" <?php selected( $instance['orderby'], 'name' ); ?>><?php esc_html_e( 'Name', 'mas-wc-brands' ); ?></option>
+						<option value="count" <?php selected( $instance['orderby'], 'count' ); ?>><?php esc_html_e( 'Count', 'mas-wc-brands' ); ?></option>
 					</select>
 				</p>
 			<?php
