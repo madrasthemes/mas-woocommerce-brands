83d6a8a5178bf16cc737a272c40e695177c34058
nilofer-farzana <nilofer@transvelo.com>
2023-10-17 11:21:09 +0530

    added HPOS compatibility

--- mas-woocommerce-brands.php
+++ mas-woocommerce-brands.php
@@ -59,3 +59,9 @@ if ( mas_wcbr_is_woocommerce_active() ) {
 	// Global for backwards compatibility.
 	$GLOBALS['mas_wc_brands'] = Mas_WC_Brands();
 }
+
+add_action( 'before_woocommerce_init', function() {
+	if ( class_exists( \Automattic\WooCommerce\Utilities\FeaturesUtil::class ) ) {
+		\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
+	}
+} );
