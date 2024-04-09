0c502a46bbae03bce9efde9d374e16ee5df4a937
Subaida Begum <subaida@transvelo.com>
2024-04-09 16:43:43 +0530

    V1.0.9

--- includes/class-mas-wc-brands.php
+++ includes/class-mas-wc-brands.php
@@ -17,7 +17,7 @@ if ( ! class_exists( 'Mas_WC_Brands' ) ) {
 		 *
 		 * @var string
 		 */
-		public $version = '1.0.6';
+		public $version = '1.0.9';
 
 		/**
 		 * The single instance of the class.
